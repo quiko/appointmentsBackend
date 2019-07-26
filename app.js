7ded56b0188b03af1fcb6f2e77fb0d0afc03a494
Joseph Morse <tamed.lionheart@gmail.com>
2019-07-26 19:52:21 -0400

    chore: moved server init to index.js

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const appointmentsRouter = require("./routes/appointments");
 const app = express();
 
 // Allow use of env vars in .env file
 require("dotenv/config");
-
-//Connect to database
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  process.env.MONGODB,
-  { useNewUrlParser: true, useCreateIndex: true },
-  () => {
-    console.log("connected to db");
-  }
-);
 
 //Middlwares
 app.use(cors());
@@ -30,8 +19,3 @@ app.disable("etag");
 
 //Routes
 app.use("/appointments", appointmentsRouter);
-
-//Start the server
-const port = process.env.PORT || 8000;
-app.listen(port);
-console.log(`Server listening at ${port}`);
